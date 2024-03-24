2f15ce15d40f14cfb4ae05399c00cdb73a52ff79
P0203K <preeti.khamkar62@gmail.com>
2024-03-24 11:19:06 +0530

    check

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const quizData = [
     {
       question: 'Question 1',
       image: 'images\\1.png',
-      options: ['Ûma‘²', 'Option 2', 'Option 3', 'Option 4'],
+      options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
       answer: 0 // Index of the correct option
     },
     {
